996a5ace9d1a10f08efbbe9775ead93090d09c00
tigreeroo <ganz67@mail.ru>
2025-03-04 14:34:27 +0300

    create relationships between user/post

--- core/models/post.py
+++ core/models/post.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models.base import Base
+
+if TYPE_CHECKING:
+    from core.models.user import User
 
 class Post(Base):
     title: Mapped[str] = mapped_column(String(100))
@@ -13,3 +18,4 @@ class Post(Base):
     user_id: Mapped[int]  = mapped_column(
         ForeignKey('users.id'),
     )
+    user: Mapped['User'] = relationship( back_populates='posts')
