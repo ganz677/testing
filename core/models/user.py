412c58d170be46f6d99c8dd06baab7373126f23a
tigreeroo <ganz67@mail.ru>
2025-03-04 14:17:42 +0300

    create usermodel

---
+++ core/models/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models.base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(32), unique=True)
+
